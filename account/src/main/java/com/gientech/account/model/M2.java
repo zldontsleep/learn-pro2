7bad79271009b7b0112af07b995f047d9528812c
zldontsleep <511089850@qq.com>
2023-04-28 14:29:02 +0800

    提交2

---
+++ account/src/main/java/com/gientech/account/model/M2.java
@@ -0,0 +1,17 @@
+package com.gientech.account.model;
+
+public class M2 {
+    private String name;
+    private String desc;
+    private int age;
+    private String email;
+    private String address;
+
+    public void m11() {
+        String str1 = "1111111";
+        String str2 = "222222";
+        String str3 = "33333";
+        String str4 = "4444";
+        String str5 = "5555";
+    }
+}
